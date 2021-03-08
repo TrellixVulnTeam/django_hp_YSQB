fd9b30a19d908bb9c08811af92926233ebea271b
Grayson-choi <784wldnd@gmail.com>
2021-03-08 17:53:59 +0900

    1일차 오후 webx 수업 끝

--- 00_django_intro_practice/articles/urls.py
+++ 00_django_intro_practice/articles/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),    
+    path('index/', views.index, name='index'),
+    path('ping/', views.ping, name='ping'),
+    path('pong/', views.pong, name='pong'),
+    path('<int:number>/', views.detail, name='detail')    
 ]
--- 00_django_intro_practice/articles/views.py
+++ 00_django_intro_practice/articles/views.py
@@ -13,3 +13,35 @@ def index(request):
         'lunch': lunch
     }
     return render(request, "index.html", context)
+
+
+def ping(request):
+    return render(request, 'ping.html')
+
+def pong(request):
+    # print(dir(request))
+    print(dir(request)) # request 안에 데이터
+    user_id = request.GET.get('id')
+    user_pw = request.GET.get('pw')
+    # 회원 가입, 로그인 했다고 가정 
+    context = {
+        'user_id': user_id,
+    } 
+    return render(request, 'pong.html', context)
+
+
+def detail(request, number):
+
+    articles = [
+        '게시물1',
+        '게시물2',
+        '게시물3',
+    ]
+
+    result = articles[number - 1]
+
+    context = {
+        'result': result
+    }
+
+    return render(request, 'detail.html', context)
