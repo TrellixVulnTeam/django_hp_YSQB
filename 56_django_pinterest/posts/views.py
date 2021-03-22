8c4ba67035628562a16f3ab46d20f94e562ea84f
Grayson-choi <784wldnd@gmail.com>
2021-03-22 10:18:25 +0900

    장고 9일차 auth 시작

--- 56_django_pinterest/posts/views.py
+++ 56_django_pinterest/posts/views.py
@@ -1,12 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .forms import PostForm
+from .models import Post
+
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
 
 @require_http_methods(["GET", "POST"])
 def create(request):
     if request.method == 'POST':
-        pass
+        form = PostForm(request.POST, request.FILES ) # File은 따로 저장되기 때문에, File 도 추가해줘야한다.
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:detail', post.pk)
     else:
-        pass
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/form.html', context)
+
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'posts/detail.html', context)
+
+
+def update(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:detail', post.pk)
+    else:
+        form = PostForm(instance=post)
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/form.html', context)
+
+def delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('posts:index')
+
+    
---
+++ 91_django_hw07_practice/articles/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles':articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/form.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'articles/detail.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/form.html', context)
+
